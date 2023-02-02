e640e6cb4b17690f198da32ea5fbbaf84f4f8d26
pyutil_experiments <48115091+Jithendrian@users.noreply.github.com>
2023-02-02 21:50:58 +0530

    Add files via upload

---
+++ tax.py
@@ -0,0 +1,49 @@
+import math
+oldRegimeIncomeTaxSlabs = [[0, 2.5], [5, 2.5], [20, 5], [30, math.inf]]
+newRegimeIncomeTaxSlabs = [[0, 3], [5, 3], [10, 3], [15, 3], [20, 3],[30,  math.inf]]
+def taxCalculation(totalSalary, regime, totalExemption):
+    if regime == 'old':
+        regimeSlabs = oldRegimeIncomeTaxSlabs
+    else:
+        regimeSlabs = newRegimeIncomeTaxSlabs
+        totalExemption = 0
+    taxableIncome = totalSalary - totalExemption
+    tax = 0
+    index = 0
+    taxableIncomeAux = taxableIncome
+    while(index < len(regimeSlabs) and taxableIncomeAux > 0):
+        if taxableIncomeAux >= regimeSlabs[index][1] * 100000:
+            tax = tax + regimeSlabs[index][1] * 100000 * regimeSlabs[index][0] * 0.01
+        else:
+            tax = tax + taxableIncomeAux * \
+                           regimeSlabs[index][0] * 0.01
+        taxableIncomeAux = taxableIncomeAux - regimeSlabs[index][1] * 100000
+        index += 1
+    return tax
+
+income_min = 800000
+income_max = 2500000
+exemption_min = 0
+exemption_max = 500000
+income_temp = income_min
+exemption_temp = 0
+x = 0
+income_exempt = {}
+while income_temp < income_max:
+    exemption_temp = 0
+    newTax = taxCalculation(income_temp, 'new', 0)
+    while exemption_temp < exemption_max and exemption_temp < income_temp:
+        exemption_temp += 5000
+        oldTax = taxCalculation(income_temp, 'old', exemption_temp)
+        if(oldTax <= newTax):
+            print(income_temp, exemption_temp, newTax, oldTax)
+            if income_temp not in income_exempt:
+                income_exempt[income_temp] = exemption_temp
+    income_temp += 100000
+
+print(income_exempt)
+for key, value in income_exempt.items():
+    print(key, value)
+
+
+
